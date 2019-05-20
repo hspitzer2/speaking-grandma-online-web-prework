ac6254f1a27c50c8a205dc8dceb3a48ea9db6b77
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-20 15:13:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
 def speak_to_grandma(greeting)
-  greeting == gets.chomp
   if greeting.lowercase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif greeting.upcase
     puts "NO, NOT SINCE 1938!"
   elsif greeting == "I LOVE YOU GRANDMA!"
-   puts "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
 

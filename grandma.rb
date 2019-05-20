f5005564a5c5034f0e341d54fc8a2ca77917f524
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-20 15:18:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_to_grandma(greeting)
-  if greeting.lowercase
+def speak_to_grandma(phrase)
+  if phrase == phrase.lowercase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif greeting.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif greeting == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end

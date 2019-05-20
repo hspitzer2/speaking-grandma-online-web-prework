d3ac2d393134b4ef2885e0526aa876befadba763
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-20 15:11:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(greeting)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif greeting.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif  greeting == "I LOVE YOU GRANDMA!"
+  elsif greeting == "I LOVE YOU GRANDMA!"
    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
